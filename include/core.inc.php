45c183138df456b4295ad565cee1806600cfde7f
f.quaglia <f.quaglia@onetag.com>
2025-05-16 13:36:01 +0200

    DB giugno 2025

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_giu2024");
+	$link = mysqli_connect("127.0.0.1", "root", "furente", "gestionale_giu2025");
 
 
 	if (!$link) {
